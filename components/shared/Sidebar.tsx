1b1d9003f023c6c21a46dd783c9e95a654d72ed0
Sudarshan S <senthilsudarshan@gmail.com>
2024-02-23 17:27:32 +0700

    work on sidebar

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -1,11 +1,12 @@
 "use client"
 
 import { navLinks } from "@/constants"
-import { SignedIn } from "@clerk/nextjs"
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
 import Image from "next/image"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 import React from "react"
+import { Button } from "../ui/button"
 
 function Sidebar() {
 	const pathName = usePathname()
@@ -35,12 +36,31 @@ function Sidebar() {
 												: "text-gray-700"
 										}`}
 									>
-										{link.label}
+										<Link className="sidebar-link" href={link.route}>
+											<Image
+												src={link.icon}
+												alt="logo"
+												width={24}
+												height={24}
+												className={`${isActive && "brightness-200"}`}
+											/>
+											{link.label}
+										</Link>
 									</li>
 								)
 							})}
+
+							<li className="flex-center cursor-pointer gap-2 p-4">
+								<UserButton afterSignOutUrl="/" showName />
+							</li>
 						</ul>
 					</SignedIn>
+					{/* When the user is Signed Out */}
+					<SignedOut>
+						<Button asChild className="button bg-purple-gradient bg-cover">
+							<Link href="/sign-in">Login</Link>
+						</Button>
+					</SignedOut>
 				</nav>
 			</div>
 		</aside>
