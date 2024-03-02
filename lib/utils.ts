956052b477c2e815fa33d1a9f5afc6b642258b6d
Sudarshan S <senthilsudarshan@gmail.com>
2024-03-02 22:40:37 +0530

    tester

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,7 +5,7 @@ import qs from "qs"
 import { twMerge } from "tailwind-merge"
 
 import { aspectRatioOptions } from "@/constants"
-import { FormUrlQueryParams } from "@/types"
+// import { FormUrlQueryParams } from "@/types"
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs))
